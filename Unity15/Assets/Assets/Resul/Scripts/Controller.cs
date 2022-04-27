607eb4dbd3b0b8c7e4746e2cab065b197120cf37
Resul KIZMAZ <algast12@gmail.com>
2022-04-28 02:10:10 +0300

    IK Looking
    Character looks wherever player looks with the camera.

--- Unity15/Assets/Assets/Resul/Scripts/Controller.cs
+++ Unity15/Assets/Assets/Resul/Scripts/Controller.cs
@@ -4,20 +4,24 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
+    [Header("Metrics")]
     public float damp;
-
     [Range(1, 20)]
     public float rotationSpeed; // 1-20 arasý dönüþ hýzý.
+    float normalFov;
+    public float sprintFov;
 
     float inputX;
     float inputY;
+    float maxSpeed;
+
+    public KeyCode sprintButton = KeyCode.LeftShift;
+    public KeyCode walkButton = KeyCode.C;
 
     public Transform model;
 
     Animator anim;
-
     Vector3 stickDirection; // Karakterin takip edeceði vector3 pozisyonu. (Bu pozisyona doðru gidecek böylece kameradan da gittiði yöne doðru karakterin döndüðü görülecek)
-
     Camera mainCam;
 
 
@@ -25,25 +29,50 @@ public class Controller : MonoBehaviour
     {
         anim = GetComponent<Animator>();
         mainCam = Camera.main;
+        normalFov = mainCam.fieldOfView;
     }
     // GÝDECEÐÝ YÖNÜ VE BU YÖNE TAKÝBÝ GERÇEKLEÞTÝRME
     private void LateUpdate()
     {
-        inputX = Input.GetAxis("Horizontal");
-        inputY = Input.GetAxis("Vertical");
-
-
-        stickDirection = new Vector3(inputX, 0, inputY);
-
         inputMove();
         inputRotation();
-
+        Movement();
     }
+    //KARAKTERÝN HANGÝ HIZDA HAREKET EDECEÐÝNÝ BELÝRLEME
+    void Movement()
+    {
+        stickDirection = new Vector3(inputX, 0, inputY);
+
+        if (Input.GetKey(sprintButton))
+        {
+            mainCam.fieldOfView = Mathf.Lerp(mainCam.fieldOfView, sprintFov, 2 * Time.deltaTime); // Karakter depar atarken hýzlanma efekti vermek için.
+
+            maxSpeed = 2;
+            inputX = 2* Input.GetAxis("Horizontal");
+            inputY = 2* Input.GetAxis("Vertical");
+
+        }else if (Input.GetKey(walkButton))
+        {
+            mainCam.fieldOfView = Mathf.Lerp(mainCam.fieldOfView, normalFov, 2 * Time.deltaTime); // Karakter depar atmazken hýzlanma efektini kaldýrmak için.
+
+            maxSpeed = 0.2f;
+            inputX = Input.GetAxis("Horizontal");
+            inputY = Input.GetAxis("Vertical");
+
+        }else
+        {
+            mainCam.fieldOfView = Mathf.Lerp(mainCam.fieldOfView, normalFov, 2 * Time.deltaTime); // Karakter depar atmazken hýzlanma efektini kaldýrmak için.
+
+            maxSpeed = 1;
+            inputX = Input.GetAxis("Horizontal");
+            inputY = Input.GetAxis("Vertical");
+        }
+    }
+
     //KARAKTERÝMÝZÝ ANÝMASYONLARLA HAREKET ETTÝRME
     void inputMove()
     {
-        anim.SetFloat("Blend", Vector3.ClampMagnitude(stickDirection, 1).magnitude, damp, Time.deltaTime * 10); // Hýzýmýzý 0-1 arasýna ortalama deðer olarak Clamp'liyoruz.
-
+        anim.SetFloat("Blend", Vector3.ClampMagnitude(stickDirection, maxSpeed).magnitude, damp, Time.deltaTime * 10); // Hýzýmýzý 0 ile belirlenen hýz arasýna ortalama deðer olarak Clamp'liyoruz.
     }
 
 
@@ -53,6 +82,5 @@ public class Controller : MonoBehaviour
         rotOfset.y = 0; // Karakter yukarý aþaðý hareket etmeyeceði için y sini 0 a eþitliyoruz.
 
         model.forward = Vector3.Slerp(model.forward, rotOfset, Time.deltaTime * rotationSpeed); //Karakterimiz de rotationOfset'e doðru dönüþ yapacak.
-
     }
 }
