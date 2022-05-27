52447da8fd3e491cb2d3626eb63ace42f803fd44
crnozs <ozsayincerenn@gmail.com>
2022-05-27 17:36:45 +0300

    Island update

---
+++ Unity15/Assets/Scripts/Restart.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+    
+}
