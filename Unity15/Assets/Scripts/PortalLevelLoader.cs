58621150dfcf21538e63d467d235fb8f4e60fb8a
crnozs <ozsayincerenn@gmail.com>
2022-05-30 17:28:27 +0300

    Island minor update

--- Unity15/Assets/Scripts/PortalLevelLoader.cs
+++ Unity15/Assets/Scripts/PortalLevelLoader.cs
@@ -1,18 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PortalLevelLoader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float levelLoadDelay = 2f;
+ 
+
+    void OnCollisionEnter(Collision other)
     {
-        
+        StartSuccessSequence();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void StartSuccessSequence()
     {
-        
+        GetComponent<Character>().enabled = false;
+        Invoke("LoadNextLevel", levelLoadDelay);
+
     }
+
+   
+
+    void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    
+
 }
