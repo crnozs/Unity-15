214a6ed2731227f03ac5cf02384801d7021cf78f
crnozs <ozsayincerenn@gmail.com>
2022-06-06 21:15:33 +0300

    Start Menu

--- Unity15/Assets/Scripts/StartButton.cs
+++ Unity15/Assets/Scripts/StartButton.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Button : MonoBehaviour
+public class StartButton : MonoBehaviour
 {
     public void LoadNextScene()
     {
